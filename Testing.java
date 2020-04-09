67be8b2b2258e1ee92773bacc04408bb60a6bc16
Rishav Bhagat <rishavb123@gmail.com>
2020-04-08 21:25:20 -0400

    updated test scripts

--- Testing.java
+++ Testing.java
@@ -1,12 +1,18 @@
 public class Testing {
     public static void main(String[] args) {
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
+        tree.add(5);
         tree.add(3);
         tree.add(2);
-        tree.add(2);
         tree.add(4);
-        
-        
+        // tree.add(7);
+
+        System.out.println(tree.preorder());
+
+        // tree.rotateRight();
+        tree.rotateLeft();
+
+        System.out.println(tree.preorder());
 
     }
 }
